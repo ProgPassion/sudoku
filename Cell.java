a9d95c8e0a4f8818bf0788074222e1ff886700e7
imran <31067252+ProgPassion@users.noreply.github.com>
2022-11-07 09:17:19 +0100

    Add files via upload

---
+++ Cell.java
@@ -0,0 +1,20 @@
+package helloworld;
+public class Cell {
+
+    private int value = 0;
+
+    public int getValue() {
+        return this.value;
+    }
+    
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    public void draw() {
+        if(value == 0) 
+            System.out.print("| |");
+        else
+            System.out.print("|" + value + "|");
+    }
+}
