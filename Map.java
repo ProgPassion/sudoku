63cbe3d099f29d56f50581b8f8576efdb83b1e0e
imran <31067252+ProgPassion@users.noreply.github.com>
2022-11-07 18:14:09 +0100

    Update Map.java

--- Map.java
+++ Map.java
@@ -2,6 +2,7 @@ package helloworld;
 public class Map {
     
     private Cell[][] map;
+    private int[][] coordinatesWriten = new int[81][3];
     
     public Map() {
         this.map = new Cell[9][9];
@@ -16,6 +17,22 @@ public class Map {
         }
     }
     
+    private void fillMapWithData() {
+        int[] rowPossibleVals = new int[9];
+        int index = 0;
+        for(int i = 9; i > 0; i--) {
+            for(int j = 0; j < 9; j++) {
+                for(int k = 0; k < 9; k++) {
+                    if(this.map[j][k].getValue() == 0) {
+                        rowPossibleVals[index] = k;
+                        index++;
+                    }
+                }
+                Math.round(Math.random() * index);
+            }
+        }
+    }    
+
     public boolean checkHorizontalLineIfNumberValid(int x, int y, int number) {
         for(int j = 0; j < 9; j++) {
             if(j != y && this.map[x][j] == number)
@@ -30,6 +47,20 @@ public class Map {
                 return false;
         }
         return true;
+    }
+
+     public boolean checkThreeByThreeGridValidation(int x, int y, int number) {
+        for(int i = x - (x % 3); i < x + 3; i++) {
+            for(int j = y - (y % 3); j < y + 3; j++) {
+                if(this.map[i][j].getValue() == number && i != x && j != y) 
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public void setCellValue(int x, int y, int number) {
+        this.map[x][y].setValue(number);
     }
 
     public void printMap() {
