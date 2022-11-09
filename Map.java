b63f8bc29a75882d0512a213d842c8dd3969f3e9
imran <31067252+ProgPassion@users.noreply.github.com>
2022-11-09 19:36:47 +0100

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,12 +1,18 @@
-package helloworld;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Map {
     
     private Cell[][] map;
-    private int[][] coordinatesWriten = new int[81][3];
-    
+    private int[][] solvedMap = new int[9][9];
+    private NumGenerator numGenerator;
+    private List<int[]> freeCellCoordinates = new ArrayList<int[]>();
+
     public Map() {
         this.map = new Cell[9][9];
         this.createMap();
+        this.numGenerator = new NumGenerator();
+        this.fillMapWithData();
     }
 
     private void createMap() {
@@ -16,26 +22,92 @@ public class Map {
             }
         }
     }
-    
+
     private void fillMapWithData() {
-        int[] rowPossibleVals = new int[9];
-        int index = 0;
-        for(int i = 9; i > 0; i--) {
+       int index = 0;
+       while(true) {
+            int randNum = (int) Math.round(Math.random() * 8) + 1;
+
+            int row = (int) Math.floor(index / 9);
+            int col = index % 9;
+            boolean rulesNotBroken = true;
+
+            if(index == 81)
+                break;
+
+            if(this.numGenerator.getNumTestedSize(index) == 9) {
+                this.numGenerator.clearSpecificNumTestedList(index);
+                this.map[row][col].setValue(0);
+                index--;
+                continue;
+            }
+            else if(this.numGenerator.checkIfNumExistsInList(randNum, index)) {
+                continue;
+            }
+            else {
+                this.numGenerator.setNumTestedToGenerator(randNum, index);
+            }
+
+            if(!this.checkHorizontalLineIfNumberValid(row, col, randNum))
+                rulesNotBroken = false;
+            if(!this.checkVerticalLineIfNumberValid(row, col, randNum))
+                rulesNotBroken = false;
+            if(!this.checkThreeByThreeGridValidation(row, col, randNum))
+                rulesNotBroken = false;
+
+            if(rulesNotBroken) {
+                this.map[row][col].setValue(randNum);
+                this.solvedMap[row][col] = randNum;
+                index++;
+            } 
+       }
+       this.removeRandomCellsValue();
+       
+    }
+
+    private void storeFreeCellsCoordinates(int x, int y) {
+        int[] coordinates = new int[2];
+        coordinates[0] = x;
+        coordinates[1] = y;
+
+        this.freeCellCoordinates.add(coordinates);
+    }
+
+    private void removeRandomCellsValue() {
+        // wipe some random cells to make the game playable
+       for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
-                for(int k = 0; k < 9; k++) {
-                    if(this.map[j][k].getValue() == 0) {
-                        rowPossibleVals[index] = k;
-                        index++;
-                    }
+                if(Math.round(Math.random() * 3) == 2 || Math.round(Math.random() * 3) == 1) {
+                    this.map[i][j].setValue(0);
+                    this.storeFreeCellsCoordinates(i, j);
                 }
-                Math.round(Math.random() * index);
             }
         }
-    }    
+    }
+    
+    public boolean checkIfSudokuIsSolved() {
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(this.map[i][j].getValue() != this.solvedMap[i][j])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean checkIfCoordsAreNotFromDefaultMap(int x, int y) {
+        for (int[] freeCellCoordinatesElem : this.freeCellCoordinates) {
+            if(freeCellCoordinatesElem[0] == x && freeCellCoordinatesElem[1] == y) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public boolean checkHorizontalLineIfNumberValid(int x, int y, int number) {
         for(int j = 0; j < 9; j++) {
-            if(j != y && this.map[x][j] == number)
+            if(j != y && this.map[x][j].getValue() == number)
                 return false;
         }
         return true;
@@ -43,15 +115,15 @@ public class Map {
 
     public boolean checkVerticalLineIfNumberValid(int x, int y, int number) {
         for(int i = 0; i < 9; i++) {
-            if(i != x && this.map[i][y] == number)
+            if(i != x && this.map[i][y].getValue() == number)
                 return false;
         }
         return true;
     }
 
-     public boolean checkThreeByThreeGridValidation(int x, int y, int number) {
-        for(int i = x - (x % 3); i < x + 3; i++) {
-            for(int j = y - (y % 3); j < y + 3; j++) {
+    public boolean checkThreeByThreeGridValidation(int x, int y, int number) {
+        for(int i = x - (x % 3); i < x - (x % 3) + 3; i++) {
+            for(int j = y - (y % 3); j < y - (y % 3) + 3; j++) {
                 if(this.map[i][j].getValue() == number && i != x && j != y) 
                     return false;
             }
@@ -62,7 +134,7 @@ public class Map {
     public void setCellValue(int x, int y, int number) {
         this.map[x][y].setValue(number);
     }
-
+    
     public void printMap() {
         System.out.print("  ");
         for(int i = 0; i < 9; i++) {
@@ -72,7 +144,7 @@ public class Map {
             System.out.println("\n  ---------------------------");
             System.out.print((i + 1) + " ");
             for(int j = 0; j < 9; j++) {
-                this.cell[i][j].draw();
+                this.map[i][j].draw();
             }
         }
         System.out.println("\n  ---------------------------");
